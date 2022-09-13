57d2a75683f3c943211104c105f31e2fe994ffdf
AglamazovaAnastasiya <aglamazova.an@gmail.com>
2022-09-13 12:35:22 +0300

    second commit

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
-step = 1
+height = float(input())
+weight = float(input())
+steps = int(input())
+time = float(input())
+
+
+step = height/4+0.37
+distance = steps*step
+V = distance/time
+energy = 0.035*weight+(V**2/height)*0.029*weight
 print(step)
