07c3e5768a8e18789299f6d0c30ba08729cc969f
AglamazovaAnastasiya <aglamazova.an@gmail.com>
2022-09-28 11:34:36 +0300

    message2

--- main.py
+++ main.py
@@ -8,4 +8,13 @@ step = height/4+0.37
 distance = steps*step
 V = distance/time
 energy = 0.035*weight+(V**2/height)*0.029*weight
-print(energy)
+kkal = energy*time
+print("Дистанция = ", distance, " сожженно ", kkal, " каллорий")
+distancekm = distance/1000
+print("Вы прошли ", distancekm, " км")
+if distancekm<=2:
+    print("Поднажми, осталось не так много")
+elif distancekm<=4:
+    print("Вы мололец! Продолжим тренировку?")
+elif distancekm>4:
+    print("Да! Вы проделали гигантскую работу!")
